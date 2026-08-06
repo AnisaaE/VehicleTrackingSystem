71d5a7e663f14b54031bd142f8564097d513a45a
Anisa <anisaemin97@gmail.com>
2026-08-06 14:51:49 +0300

    Load local appsettings overrides

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,6 +7,8 @@ using VehicleTrackingSystem.TrackingProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
